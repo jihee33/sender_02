9f9f8a68fc8552793d6b5eed4780644d442034d0
James <taho1897@gmail.com>
2016-09-19 17:34:10 +0900

    Naver 로그인 추가

--- routes/chatting.js
+++ routes/chatting.js
@@ -80,41 +80,6 @@ router.post('/', isAuthenticated, isActivated, function(req, res, next) {
              });
          });
     }
-    /*if(req.url.match(/\/\?action=notification/i)) { // No.23 배송 알림 전송하기
-        var receiverId = parseInt(req.body.receiver_id);
-        logger.log('debug', 'receiverId : %s', receiverId);
-        Chatting.getRegistrationToken(receiverId, function(err, result) {
-           if (err) {
-               return next(err);
-           }
-            var tokens = [];
-            logger.log('debug', 'reg_token : %j', result, {});
-            tokens.push(result.registration_token);
-            logger.log('debug', 'tokens : %j', tokens, {});
-            var message = new fcm.Message({ // 위에서 가져오거나 여기서 바로 만들거나
-                data: {
-                    type : 'delivery'
-                }
-            });
-            logger.log('debug', 'fcm message : ', message);
-            var sender = new fcm.Sender(process.env.GCM_KEY);
-            sender.send(message, {registrationTokens: tokens}, function (err, response) {
-                if (err) {
-                    return next(err);
-                }
-                logger.log('debug', 'response : %j', response, {});
-                if (response.failure !== 1) {
-                    res.send({
-                        result: '배송 요청 전송을 성공하였습니다.'
-                    });
-                } else {
-                    res.send({
-                        error: '배송 요청 전송을 실패하였습니다.'
-                    });
-                }
-            });
-        });
-    }*/
 });
 
 router.get('/', isSecure, isAuthenticated, isActivated, function(req, res, next) {
